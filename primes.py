5af47e10f1dd2421e1152291c64d3ba2737ce543
Quan Tran <quanabcis2003@gmail.com>
2022-10-04 20:00:15 +0100

    Solve exercise

--- primes.py
+++ primes.py
@@ -2,5 +2,25 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError("Parameter must be positive.")
+
     list = []
+    count = 0
+    num = 3
+    
+    list.append(2)
+
+    while count < number_of_primes - 1:
+        if is_prime(num):
+            list.append(num)
+            count += 1
+        num += 1
+
     return list
+
+def is_prime(num):
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            return False
+    return True
